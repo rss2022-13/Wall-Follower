13cce77642541543a0089c55a6165364c0a77579
jmanius1 <jgeorgeakpenyi@gmail.com>
2022-03-01 23:17:12 -0500

    Limiting front_wall_theta

--- wall_follower_Ivory_modified.py
+++ wall_follower_Ivory_modified.py
@@ -105,7 +105,7 @@ class WallFollower:
         msg.drive.acceleration = 0
         msg.drive.steering_angle = P*error + D*derivative + max(min(I*self.integral, 0.34), -0.34)
 
-        if abs(front_b*np.cos(front_theta) < 2*self.DESIRED_DISTANCE):
+        if abs(front_b*np.cos(front_theta) < 2*self.DESIRED_DISTANCE) and abs(front_theta) < 0.1:
             msg.drive.steering_angle = 2*fsf*0.34
 
         msg.drive.steering_angle_velocity = 0
